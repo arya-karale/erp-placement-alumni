bac062043e11c0460f8d89e68cd196f639b8b19a
Ainapure Arya Hemant <ainapurearya4@gmail.com>
2026-03-19 22:45:22 +0530

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.database import Base, engine
-from app.routes import admin_company, student
+from app.routes import admin_company, student, admin_drive, admin_round
 
 app = FastAPI(title="College ERP – Placement Module")
 
@@ -8,7 +8,9 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(admin_company.router)
 app.include_router(student.router)
+app.include_router(admin_drive.router)
+app.include_router(admin_round.router)
 
 @app.get("/")
 def root():
-    return {"message": "ERP Placement Backend is running"}
+    return {"message": "ERP Placement Backend is running 🚀"}
